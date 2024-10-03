3df86d1a076ca2c0b95b909ffc37b9298df2f1d0
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-10-04 00:42:39 +0100

    update

--- src/main/java/com/example/bookstoreapi/config/SecurityConfig.java
+++ src/main/java/com/example/bookstoreapi/config/SecurityConfig.java
@@ -1,23 +1,19 @@
-package com.example.bookstoreapi.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers(new AntPathRequestMatcher("/api/public/**")).permitAll() // Use antMatchers for URL patterns
-            .anyRequest().authenticated()
-            )
+            .csrf()
+            .disable()
+            .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated())
             .httpBasic();
         
         return http.build();
--- src/main/java/com/example/bookstoreapi/model/Book.java
+++ src/main/java/com/example/bookstoreapi/model/Book.java
@@ -1,7 +1,13 @@
 package com.example.bookstoreapi.model;
 
-import javax.persistence.*;
 import java.time.Year;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
 
 @Entity
 @Table(name = "books")
